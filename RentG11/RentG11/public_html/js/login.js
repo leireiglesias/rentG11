9a2ca015e45168b9973508d9e450a987124ca839
USUARIO <USUARIO@192.168.1.107>
2018-11-28 07:02:51 +0100

    Cambios en la validación

--- RentG11/RentG11/public_html/js/login.js
+++ RentG11/RentG11/public_html/js/login.js
@@ -10,31 +10,32 @@ function iniciar() {
     email_log = document.getElementById("correoLogin");
     contraseña_log = document.getElementById("passLogin");
     var boton = document.getElementById("login");
-    email_log.addEventListener("input", validacionEmail);
-    contraseña_log.addEventListener("input", validacionContraseña);
+    formulario = document.querySelector("form[name='frmLogin']");
+    formulario.addEventListener("invalid", validacion, true);
+    formulario.addEventListener("input", comprobar);
     crearbd();
-    validacionEmail();
-    validacionContraseña();
-    boton.addEventListener("click", validar);
+    boton.addEventListener("click", enviarformulario);
 }
-function validacionEmail() {
-    if (email_log.value === "") {
-        email_log.setCustomValidity("Inserte su email");
-    } else {
-        email_log.setCustomValidity("");
+
+function enviarformulario() {
+    var valido = formulario.checkValidity();
+    if (valido) {
+        formulario.submit();
     }
 }
-function validacionContraseña() {
-    if (contraseña_log.value === "") {
-        contraseña_log.setCustomValidity("Inserte su contraseña");
+function comprobar(evento) {
+    var elemento = evento.target;
+    if (elemento.validity.valid) {
+        elemento.style.background = "#FFFFFF";
     } else {
-        contraseña_log.setCustomValidity("");
+        elemento.style.background = "#FFDDDD";
     }
 }
+
 function crearbd() {
     DB = indexedDB.open("RentG11", 1);
     DB.onsuccess = function (e) {
-        
+
 
     };
 
